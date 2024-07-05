0d3f771b58ad90ac462dabae2fa7f5bb0c139bb8
Katlego Maredi <katlegomaredi9@gmail.com>
2024-07-05 19:41:07 +0200

    Using openweather api

--- tutorials/tut1_weather_app/src/app/page.tsx
+++ tutorials/tut1_weather_app/src/app/page.tsx
@@ -1,6 +1,75 @@
+"use client";
 import Navbar from "@/components/Navbar";
+import axios from "axios";
+import { useQuery } from "react-query";
 
+interface WeatherDetail {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherDetail[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>("repoData", async () => {
+    console.log(process.env.OPEN_WEATHER_API_KEY)
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Katlehong&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+    return data;
+  }
+    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=Katlehong&appid=7b8e35e64de1218e0868e8fcb0a95e03`)
+    //   .then((res) => res.json())
+  );
+  console.log("data", data);
+  if (isLoading) return "Loading.....";
+
+  // https://api.openweathermap.org/data/2.5/weather?q=Katlehong&appid=7b8e35e64de1218e0868e8fcb0a95e03`
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
