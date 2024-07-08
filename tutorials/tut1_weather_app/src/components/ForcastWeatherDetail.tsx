0d6e35c242b76444dd65c2e3f1c895017f1ffdcc
Katlego Maredi <katlegomaredi9@gmail.com>
2024-07-08 23:58:18 +0200

    Create 7 day forcast(Not done)

--- tutorials/tut1_weather_app/src/components/ForcastWeatherDetail.tsx
+++ tutorials/tut1_weather_app/src/components/ForcastWeatherDetail.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Container from './Container'
+import WeatherIcon from './WeatherIcon'
 
 type Props = {}
 
 export default function ForcastWeatherDetail({}: Props) {
   return (
-    <Container></Container>
+    <Container className='gap-4'>
+        <section className='flex gap-4 items-center px-4'>
+<div>
+    <WeatherIcon/>
+</div>
+        </section>
+    </Container>
   )
 }
