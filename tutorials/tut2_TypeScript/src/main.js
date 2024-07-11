96ffb5916421b3eb22b4371970f03e63645e5923
Katlego Maredi <katlegomaredi9@gmail.com>
2024-07-12 00:01:12 +0200

    Unions and Type aliases

--- tutorials/tut2_TypeScript/src/main.js
+++ tutorials/tut2_TypeScript/src/main.js
@@ -1,27 +1,10 @@
-// Variables
-var hello = 'wolrd';
-var strHello = 'world';
-//Specify type
-var intNumber = 3;
-// Functions
-var getFullName = function (name, surname) {
-    return name + ' ' + surname;
-};
-getFullName("Katlego", "Maredi");
-// Objects
-var userObj = {
-    name: "Moster",
-    age: 30
-};
-var userObj2 = {
-    name: "Moster",
-    age: 30
-};
-var userObj3 = {
-    name: "Fez",
-    age: 23,
-    getMessage: function () {
-        return "Hello " + name;
-    },
-};
-console.log(userObj3.age, userObj3.name);
+// // Variables
+// const hello = 'wolrd';
+// let strHello = 'world';
+var username = 'fez';
+var pageName = '1';
+var errMsg = null;
+var user = null;
+var id = 'Fax';
+var popularTag = ["Dragon", "Coffe"];
+var dragonsTag = null;
--- tutorials/tut2_TypeScript/src/main.ts
+++ tutorials/tut2_TypeScript/src/main.ts
@@ -1,42 +1,65 @@
-// Variables
-const hello = 'wolrd';
-let strHello = 'world';
+// // Variables
+// const hello = 'wolrd';
+// let strHello = 'world';
 
-//Specify type
-let intNumber: number = 3;
+// //Specify type
+// let intNumber: number = 3;
 
-// Functions
-const getFullName = (name: string, surname: string): string => {
-    return name + ' ' + surname;
-}
+// // Functions
+// const getFullName = (name: string, surname: string): string => {
+//     return name + ' ' + surname;
+// }
 
-getFullName("Katlego", "Maredi");
+// getFullName("Katlego", "Maredi");
 
-// Objects
-const userObj: { name: string, age: number } = {
-    name: "Moster",
-    age: 30
-}
+// // Objects
+// const userObj: { name: string, age: number } = {
+//     name: "Moster",
+//     age: 30
+// }
 
-const userObj2: { name: string, age: number } = {
-    name: "Moster",
-    age: 30
-}
+// const userObj2: { name: string, age: number } = {
+//     name: "Moster",
+//     age: 30
+// }
 
-// Interfaces
-interface User {
+// // Interfaces
+// interface User {
+//     name: string
+//     age?: number
+//     //Use '?' to indicate that it is not required
+//     getMessage(): string
+// }
+
+// const userObj3: User = {
+//     name: "Fez",
+//     age: 23,
+//     getMessage() {
+//         return "Hello " + name
+//     },
+// }
+
+// console.log(userObj3.age, userObj3.name,userObj3.getMessage());
+
+// Union operator
+interface UserInterace {
     name: string
-    age?: number
-    //Use '?' to indicate that it is not required
-    getMessage():string
+    surname: string
+    age: number
 }
 
-const userObj3:User={
-    name:"Fez",
-    age:23,
-    getMessage() {
-        return "Hello "+name
-    },
-}
+let username: string = 'fez';
+let pageName: string | number = '1';
+let errMsg: string | null = null;
 
-console.log(userObj3.age,userObj3.name);
+let user: UserInterace | null = null;
+
+// Type alises
+type ID = string;
+type PopularTag = string;
+type MaybePopularTag=PopularTag|null;
+
+let id: ID = 'Fax';
+
+const popularTag: PopularTag[] = ["Dragon", "Coffe"];
+const dragonsTag:MaybePopularTag=null;
