b105e12c2a9cc508330a43006a19a2ead75104b6
Katlego Maredi <katlegomaredi9@gmail.com>
2024-07-11 16:20:37 +0200

    Next weather app (Complete)

---
+++ tutorials/tut1_weather_app/src/app/atom.ts
@@ -0,0 +1,5 @@
+import { atom } from "jotai";
+
+export const placeAtom = atom("Katlehong");
+
+export const loadingCityAtom=atom(false);
--- tutorials/tut1_weather_app/src/app/stom.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from "jotai";
-
-export const placeAtom=atom("Republic Of India");
