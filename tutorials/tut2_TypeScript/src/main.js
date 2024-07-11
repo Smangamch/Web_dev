75f1e2c9c6de354498bfcac635a48807b5f7119c
Katlego Maredi <katlegomaredi9@gmail.com>
2024-07-11 23:46:54 +0200

    interfaces in typescript

--- tutorials/tut2_TypeScript/src/main.js
+++ tutorials/tut2_TypeScript/src/main.js
@@ -8,3 +8,20 @@ var getFullName = function (name, surname) {
     return name + ' ' + surname;
 };
 getFullName("Katlego", "Maredi");
+// Objects
+var userObj = {
+    name: "Moster",
+    age: 30
+};
+var userObj2 = {
+    name: "Moster",
+    age: 30
+};
+var userObj3 = {
+    name: "Fez",
+    age: 23,
+    getMessage: function () {
+        return "Hello " + name;
+    },
+};
+console.log(userObj3.age, userObj3.name);
--- tutorials/tut2_TypeScript/src/main.ts
+++ tutorials/tut2_TypeScript/src/main.ts
@@ -11,3 +11,32 @@ const getFullName = (name: string, surname: string): string => {
 }
 
 getFullName("Katlego", "Maredi");
+
+// Objects
+const userObj: { name: string, age: number } = {
+    name: "Moster",
+    age: 30
+}
+
+const userObj2: { name: string, age: number } = {
+    name: "Moster",
+    age: 30
+}
+
+// Interfaces
+interface User {
+    name: string
+    age?: number
+    //Use '?' to indicate that it is not required
+    getMessage():string
+}
+
+const userObj3:User={
+    name:"Fez",
+    age:23,
+    getMessage() {
+        return "Hello "+name
+    },
+}
+
+console.log(userObj3.age,userObj3.name);
